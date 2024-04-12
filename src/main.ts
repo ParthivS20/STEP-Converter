420943fd11aeb75e5b026004e1e4e5468297c3e3
Parthiv Shah <parthivjs12@gmail.com>
2024-04-11 19:45:31 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,20 @@ const outFiles = getInput('out-files', { required: true }).split(/\r\n|\r|\n/g)
 let needMayo = false
 
 outFiles.forEach((file) => {
-  if (!FORMATS.includes(file)) {
+    if (!FORMATS.includes(file)) {
     throw new Error(`Invalid out-file: ${file}`)
-  }
+    }
 
-  if (!needMayo && (file === 'step' || file === 'stl' || file === 'png')) {
-    needMayo = true
-    exec('sh', [`bin/install-mayo.sh`]).then(() => {
-        console.log("mayo installed")
-    }).catch((error) => {
-        console.error("mayo failed")
-        throw error
-    })
-  }
+    if (!needMayo && (file === 'step' || file === 'stl' || file === 'png')) {
+        needMayo = true
+        console.log("mayo needed")
+        exec('sh', [`bin/install-mayo.sh`]).then(() => {
+            console.log("mayo installed")
+        }).catch((error) => {
+            console.error("mayo install failed")
+            throw error
+        })
+    }
 })
 
 const outDir = getInput('out-dir', { required: true })
@@ -29,4 +30,10 @@ const outDir = getInput('out-dir', { required: true })
 if (!fs.existsSync(outDir)) {
   fs.mkdirSync(outDir, { recursive: true })
 }
+
+//loop through all files in mair directory
+fs.readdirSync(outDir).forEach(file => {
+    //if the file is not in the outFiles array, delete it
+    console.log(file)
+})
 
