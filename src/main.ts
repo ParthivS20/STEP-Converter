cb3e8d7b4920a85cdbbe678b64b2cb903e49e43c
Parthiv Shah <parthivjs12@gmail.com>
2024-04-11 19:31:19 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { getInput } from '@actions/core'
+import { exec } from '@actions/exec'
 import * as fs from 'fs'
 
-if (!fs.existsSync('docs')) {
-  fs.mkdirSync('docs')
-}
+const FORMATS = ['sch_pdf', 'pcb_pdf', 'png', 'stl', 'step']
 
 const outFiles = getInput('out-files', { required: true }).split(/\r\n|\r|\n/g)
-console.log(outFiles)
+
+outFiles.forEach((file) => {
+  if (!FORMATS.includes(file)) {
+    throw new Error(`Invalid out-file: ${file}`)
+  }
+
+  if (file === 'step' || file === 'stl' || file === 'png') {
+    exec(`mayo.sh`)
+    console.log("mayo installed")
+  }
+})
+
+const outDir = getInput('out-dir', { required: true })
+
+if (!fs.existsSync(outDir)) {
+  fs.mkdirSync(outDir, { recursive: true })
+}
+
