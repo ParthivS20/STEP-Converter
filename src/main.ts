f17129643751a13ea25d4a4264cd9be838e4a4a9
Parthiv Shah <parthivjs12@gmail.com>
2024-04-11 20:05:59 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -8,43 +8,40 @@ const outFiles = getInput('out-files', { required: true }).split(/\r\n|\r|\n/g)
 const outDir = getInput('out-dir', { required: true });
 
 console.log(1);
-(async () => {
-    let needMayo = false
-    console.log(2);
-    outFiles.forEach((file) => {
-        if (!FORMATS.includes(file)) {
-            throw new Error(`Invalid out-file: ${file}`)
-        }
-
-        if (!needMayo && (file === 'step' || file === 'stl' || file === 'png')) {
-            needMayo = true
-        }
-    })
-
-    console.log(3);
-    if (needMayo) {
-        await exec('sh', [`bin/install-mayo.sh`]).then(() => {
-                console.log("mayo installed")
-            }).catch((error) => {
-                console.error("mayo install failed")
-                throw error
-            })
-    }
-    console.log(4);
-
-    if (!fs.existsSync(outDir)) {
-        fs.mkdirSync(outDir, { recursive: true })
+let needMayo = false
+console.log(2);
+outFiles.forEach((file) => {
+    if (!FORMATS.includes(file)) {
+        throw new Error(`Invalid out-file: ${file}`)
     }
 
-    console.log(5);
+    if (!needMayo && (file === 'step' || file === 'stl' || file === 'png')) {
+        needMayo = true
+    }
+})
 
-    //loop through all files in mair directory
-    fs.readdirSync(outDir).forEach(file => {
-        //if the file is not in the outFiles array, delete it
-        console.log(file)
+console.log(3);
+if (needMayo) {
+    exec('sh', [`bin/install-mayo.sh`]).then(() => {
+        console.log("mayo installed")
+    }).catch((error) => {
+        console.error("mayo install failed")
+        throw error
     })
+}
+console.log(4);
 
-    console.log(6);
-})()
+if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir, { recursive: true })
+}
 
+console.log(5);
+
+//loop through all files in mair directory
+fs.readdirSync(outDir).forEach(file => {
+    //if the file is not in the outFiles array, delete it
+    console.log(file)
+})
+
+console.log(6);
 
