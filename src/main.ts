1387ad70b9096c9d2ad9b9fc6b1469f7fb706c8f
Parthiv Shah <parthivjs12@gmail.com>
2024-04-11 22:54:13 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ console.log(ACTION_PATH);
         } else if (fileName.endsWith("kicad_pcb")) {
             let basename = fileName.replace(".kicad_pcb", "")
             if (outFiles.includes("step")) {
-                exec('kicad-cli', ['pcb', 'export', 'step', '--subst-models', `-o ${outDir}/${basename}.step`, fileName]).then(() => {
+                exec('kicad-cli', ['pcb', 'export', 'step', '--subst-models', `-o "${outDir}/${basename}.step"`, `"${fileName}"`]).then(() => {
                     console.log(`STEP export success: ${fileName}`)
                 }).catch((error) => {
                     console.error("step export failed")
@@ -61,14 +61,14 @@ console.log(ACTION_PATH);
                 })
             } else if (fs.existsSync(`${outDir}/${basename}.step`)) {
                 if (outFiles.includes("stl")) {
-                    exec('./mayo.AppImage', [`${outDir}/${basename}.step`, `-e ${outDir}/${basename}.stl`]).then(() => {
+                    exec('./mayo.AppImage', [`"${outDir}/${basename}.step"`, `-e "${outDir}/${basename}.stl"`]).then(() => {
                         console.log(`STL export success: ${fileName}`)
                     }).catch((error) => {
                         console.error("stl export failed")
                         throw error
                     })
                 } else if (outFiles.includes("png")) {
-                    exec('./mayo.AppImage', [`-s ${ACTION_PATH}/mayo-config.ini`, `${outDir}/${basename}.step`, `-e ${outDir}/${basename}.png`]).then(() => {
+                    exec('./mayo.AppImage', [`-s "${ACTION_PATH}/mayo-config.ini"`, `"${outDir}/${basename}.step"`, `-e "${outDir}/${basename}.png"`]).then(() => {
                         console.log(`PNG export success: ${fileName}`)
                     }).catch((error) => {
                         console.error("png export failed")
