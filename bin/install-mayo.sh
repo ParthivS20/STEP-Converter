48c2469f97530b703949d5d489594a73f4bcd3bc
Parthiv Shah <parthivjs12@gmail.com>
2024-04-11 22:44:05 -0500

    initial commit

--- bin/install-mayo.sh
+++ bin/install-mayo.sh
@@ -1,2 +1,4 @@
+sudo apt-get update
+sudo apt-get install xvfb wget libfuse2
 wget -O ./mayo.AppImage https://github.com/fougue/mayo/releases/download/v0.8.0/Mayo-0.8.0-x86_64.AppImage
 chmod a+x mayo.AppImage
